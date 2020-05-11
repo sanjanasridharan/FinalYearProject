dd642a103162fa55edcab0e789aedc47e7dec777
sanjanasridharan <sanju1997ss@gmail.com>
2020-05-11 21:22:49 +0530

    added list

--- ReactApp/my-app/src/components/ListOfSym.js
+++ ReactApp/my-app/src/components/ListOfSym.js
@@ -5,20 +5,34 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 export default function ListOfSym({props}) {
-  console.log(props)
+  const [arr,setArr]=React.useState([])
+ const displaySym = (props) => {
+   
+   if(props.e.target.checked)
+       arr.push(props.sym)
+       
+  if(!props.e.target.checked)
+     {let i=arr.indexOf(props.sym)
+       arr.splice(i,i)
+     }
+    
+  };
     return (
       <FormControl  component="fieldset">
         {/* <FormLabel style={{padding:'20px'}} component="legend">Select the symptoms:</FormLabel> */}
         <FormGroup style={{padding:'20px'}} aria-label="position" row>
          {props.map((sym) =>(
           <FormControlLabel
+            draggable
             value="end"
-            control={<Checkbox color="primary" />}
+            control={<Checkbox color="primary"  />}
             label={sym}
             labelPlacement="end"
-          />))}
-          
+            onClick={(e => displaySym({e,sym}))}
+            onDrag={(event) => console.log(event)}
             
+            
+          />))} 
         </FormGroup>
       </FormControl>
     );
--- ReactApp/my-app/src/components/MainPage.js
+++ ReactApp/my-app/src/components/MainPage.js
@@ -1,10 +1,9 @@
 import React,{useState} from 'react'
 import MenuAppBar from './MenuAppBar'
 import ListOfSym from './ListOfSym'
-import ComboBox from './EnterSymptom'
-import axios from 'axios'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
 
 function MainPage() {
@@ -13,6 +12,7 @@ function MainPage() {
     const [displayDis,setDis]=useState(false)
     const [symp,setSymptom] = useState('')
     const [SympList,setList]=useState([])
+    const [value, setValue] = React.useState();
    
     const getSymptom = () => {
      console.log(symp)
@@ -23,7 +23,7 @@ function MainPage() {
         'Accept': 'application/json'
        },
        body: JSON.stringify({
-        'symptom': symp})
+        'symptom': value.title})
 
     }).then(res => res.json()).then(data => {
       console.log(data.time);
@@ -31,16 +31,27 @@ function MainPage() {
     });
     setSym(true)
     }
+
+
       function getDisease() {     
         setDis(true)
        }
+       
     return (
         <div>
              <MenuAppBar props={t}/>
-             
-             {/* <ComboBox/>
-              */}
-              <TextField variant="filled"  margin="normal" required fullWidth  label="Enter Symptom"  value={symp}  autoFocus onChange={e => setSymptom(e.target.value)} />
+              <Autocomplete
+                    id="controllable-states-demo"
+                    value={value}
+                    onChange={(event, newValue) => {
+                     setValue(newValue);
+                             }}
+                    options={top100Sym}
+                    getOptionLabel={(option) => option.title}
+                    style={{ width: 300,marginTop:'120px',marginLeft:'600px'}}
+                    renderInput={(params) => <TextField {...params} label="Enter Symptom" variant="outlined" />}
+    />
+    
              <Button style={{marginTop:'20px',marginRight:'96px',width:'300px'}}variant="contained" color="primary" onClick={getSymptom}>
               Predict similar symptoms
              </Button>
@@ -53,7 +64,7 @@ function MainPage() {
                </div>
                <Button style={{marginTop:'20px',marginRight:'96px',width:'300px'}}variant="contained" color="primary" onClick={getDisease}>
               Predict Disease
-             </Button>
+                </Button>
                {displayDis ?
                <div><div style={{marginTop:'20px',marginRight:'247px'}}> <h>The Predicted Disease:</h>
                <div style={{marginLeft:'180px',marginTop:'20px'}}>
@@ -77,5 +88,108 @@ function MainPage() {
         </div>
     )
 }
+const top100Sym = [
+  { title: 'shortness of breath' },
+  { title: 'pain' },
+  { title: 'fever'},
+  { title: 'pain abdominal'},
+  { title: 'diarrhea'},
+  { title: "vomiting" },
+  { title: 'asthenia'},
+  { title: 'cough' },
+  { title: 'dyspnea'},
+  { title: 'nausea'},
+  { title: 'unresponsiveness' },
+  { title: 'chill' },
+  { title: 'Forrest Gump' },
+  { title: 'pain chest'},
+  { title: 'apyrexial' },
+  { title: "decreased body weight" },
+  { title: 'agitation'},
+  { title: 'rale' },
+  { title: 'lesion'},
+  { title: 'mass of body structure'},
+  { title: 'hypotension' },
+  { title: 'sore to touch' },
+  { title: 'hallucinations auditory'},
+  { title: "night sweat" },
+  { title: 'orthopnea' },
+  { title: 'syncope'},
+  { title: 'thicken'},
+  { title: 'haemorrhage'},
+  { title: 'swelling'},
+  { title: 'tremor' },
+  { title: 'distress respiratory'},
+  { title: 'feeling suicidal' },
+  { title: 'hypokinesia' },
+  { title: 'patient non compliance' },
+  { title: 'suicidal'},
+  { title: 'feeling hopeless'},
+  { title: 'irritable mood'},
+  { title: 'sleepy'},
+  { title: 'sweating increased'},
+  { title: 'tachypnea'},
+  { title: 'wheezingt' },
+  { title: 'worry'},
+  { title: 'ascites' },
+  { title: 'blackout' },
+  { title: 'difficulty' },
+  { title: 'dyspnea on exertion' },
+  { title: 'headache'},
+  { title: 'hemiplegia' },
+  { title: 'hyponatremia' },
+  { title: 'non-productive cough' },
+  { title: 'pleuritic pain'},
+  { title: 'pruritus' },
+  { title: 'seizure	'},
+  { title: 'sleeplessness' },
+  { title: 'angina pectoris' },
+  { title: 'constipation' },
+  { title: 'facial paresis'},
+  { title: 'fall' },
+  { title: 'fatigue'},
+  { title: 'hallucinations visual'},
+  { title: 'hemodynamically stable' },
+  { title: 'hyperkalemia' },
+  { title: 'mental status changes' },
+  { title: 'palpitation'},
+  { title: 'productive cough' },
+  { title: 'anorexia' },
+  { title: 'bradycardia' },
+  { title: 'chest tightness' },
+  { title: 'dizziness' },
+  { title: 'guaiac positive' },
+  { title: 'homelessness' },
+  { title: 'prostatism' },
+  { title: 'tumor cell invasion' },
+  { title: 'abdominal tenderness' },
+  { title: 'abscess bacterial' },
+  { title: 'chest discomfort' },
+  { title: 'consciousness clear'},
+  { title: 'decreased translucency' },
+  { title: 'distended abdomen' },
+  { title: 'erythema' },
+  { title: 'jugular venous distention'},
+  { title: 'lethargy' },
+  { title: 'mood depressed' },
+  { title: 'myalgia' },
+  { title: 'redness' },
+  { title: 'rhonchus'},
+  { title: 'transaminitis' },
+  { title: 'unconscious state' },
+  { title: 'unsteady gait' },
+  { title: 'weepiness' },
+  { title: 'breath sounds decreased'},
+  { title: 'dysarthria'},
+  { title: "hematuria"},
+  { title: 'intoxication'},
+  { title: 'muscle twitch'},
+  { title: 'nightmare' },
+  { title: 'numbness' },
+  { title: 'pressure chest' },
+  { title: 'sinus rhythm'},
+  { title: 'yellow sputum'},
+  { title: 'verbal auditory hallucinations'},
+];
 
 export default MainPage
